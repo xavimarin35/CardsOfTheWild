979ff6b636d7949ae1bb584fa94a716722f3e400
Xavi Marín Solà <xmarin98@gmail.com>
2021-01-19 12:55:17 +0100

    CardsOfTheWild & Barracuda added

---
+++ CardsOfTheWild_A3/Assets/Scripts/Agent.cs
@@ -0,0 +1,272 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Barracuda;
+
+public class Agent : MonoBehaviour
+{
+	// Neural network: asset and worker
+	public NNModel CNN_Model_Asset;
+	private Model model;
+	private IWorker worker;
+
+	// Render textures (enemy's cards)
+	public RenderTexture [] renderTextures;
+
+	private int imgWidth, imgHeight;
+
+	// Game parameters
+	private int numEnemyCards;
+	private int [] myCards;
+	private int NUM_CLASSES = 3;
+	private int DECK_SIZE   = 4;
+
+	// States and Q-table
+	private int enemyCombinations;
+	private int deckCombinations;
+	private int numActions;
+	private int state, oldState;
+	private int action;
+	private float[, ] qTable;
+
+
+	// RL management
+
+	// Update rate of learned values
+	private float learningRate = 0.5f;	
+	// Discount factor when computing the Q values
+	// Note: Usually it is 0.9-0.99, but here the next state is random and is not relevant, so it is not used
+	//private float gamma = 0.0f;    
+	// Starting epsilon (choose random actions)
+	private float epsilon = 1.0f;
+	// Minimum epsilon (after coolingSteps)
+	private float minEpsilon = 0.1f;
+	// Factor to reduce epsilon (number of actions that it
+	// takes it to go down to minEpsilon)
+	private int coolingSteps = 1000;
+
+
+
+	// Not using Start() so we are positive that this is run before the agent is asked to play
+    public void Initialize()
+    {
+
+    	imgWidth  = renderTextures[0].width;
+    	imgHeight = renderTextures[0].height;
+
+    	// Create the neural network
+    	model  = ModelLoader.Load(CNN_Model_Asset, false);
+        worker = WorkerFactory.CreateWorker(WorkerFactory.Type.CSharp, model, false);
+        Debug.Log("started"); 
+
+        // Gather other game information
+        numEnemyCards = renderTextures.Length;
+
+        myCards = new int[NUM_CLASSES];  // counts how many cards of each class the agent has
+
+        enemyCombinations = Mathf.FloorToInt(Mathf.Pow(NUM_CLASSES, numEnemyCards));      
+        deckCombinations  = Mathf.FloorToInt(Mathf.Pow(DECK_SIZE+1, myCards.Length-1));      
+
+        // The max number of possible actions is the same as the number of enemy combinations
+        // (NUM_CLASSES ^ numCards)
+        numActions = enemyCombinations;
+
+        // Generate the Q-table 
+        qTable = new float[enemyCombinations*deckCombinations, numActions];
+    }
+
+
+    // Play one turn
+    // deck: the cards the player has to play with
+    // enemyChars: the labels of the enemy's characters (for debug/evaluation purposes only)
+    public int [] Play(int [] deck, int [] enemyChars) 
+    {
+    	// First: read the enemy cards
+
+    	int [] enemyCards = new int[numEnemyCards];
+
+
+    	for(int i=0; i<renderTextures.Length; i++)
+    	{
+    		enemyCards[i] = ClassifyCard(renderTextures[i]);
+
+    		//Debug.Log("Agent.Play - classified card: " + enemyCards[i].ToString() + " -> " + enemyChars[i].ToString());
+    	}
+
+
+    	//foreach(int x in enemyCards)
+    	//	Debug.Log(x);
+
+    	// Second: compute the state of the game
+    	CountMyCards(deck);
+
+    	oldState = state;
+    	state    = ComputeState(enemyCards);
+
+    	// Third: choose an action
+    	action = ChooseAction();
+
+    	//Debug.Log("ACTION");
+    	//Debug.Log(action);
+
+
+    	// Translate the action to cards and return
+    	return ActionToCards(action);
+    }
+
+
+    // Get the reward obtained from the current hand; 
+    // update q-table accordingly
+    // Note: the next state is not relevant here, as it is completely random
+    public void GetReward(float reward)
+    {
+		qTable [state, action] += learningRate * (reward - qTable [state, action]); 
+    }
+
+
+    // PRIVATE 
+
+    // Return character in the given card (0-2)
+    private int ClassifyCard(RenderTexture rtex)
+    {
+        // Barracuda expects NHWC (batch, height, width, channels) as image format
+        // but it also accepts a RenderTexture directly, much easier here
+        // Important: the pixel values must are in [0, 1] (the NN must be trained
+        // with that range too)
+
+        Tensor input = new Tensor(rtex, 3);  
+        worker.Execute(input);
+        var res = worker.PeekOutput();
+
+        // This gives the class of the card
+		float[] output = res.AsFloats();
+
+        input.Dispose();
+
+        return argmax(output);
+    }
+
+    private int argmax(float [] values)
+    {
+    	float maxVal = values[0];
+    	int   maxIdx = 0;
+
+    	for(int i=1; i<values.Length; i++)
+    		if(values[i] > maxVal)
+    		{
+    			maxVal = values[i];
+    			maxIdx = i;
+    		}
+
+    	return maxIdx;
+    }
+
+    
+    // Given an array of cards [0, 1, 2, 1] that encodes the cards drawn by the player, 
+    // set array myCards with the amount of cards of each class
+    private void CountMyCards(int [] deck) 
+    {
+    	for(int i=0; i<myCards.Length; i++)
+    		myCards[i] = 0;
+
+    	for(int i=0; i<deck.Length; i++)
+    		myCards[deck[i]]++;
+
+    	//Debug.Log("ExtractMyCards");
+    	//foreach(int x in myCards)
+    	//	Debug.Log(x);
+
+    }
+
+
+    // Compute the state of the game given the cards of the enemy and
+    // the deck of cards of the player
+    // Idea: 
+    // enemy = all combinations of enemy cards (NUM_CLASSES^numCards)
+    // player = number of cards of each class but the last one
+    // (as it will be the remaining number of cards, so always the same
+    // given the first two)
+    // state = find all combinations of enemy and player
+    private int ComputeState(int [] enemyCards)
+    {
+    	int enemy = 0;
+    	foreach(int card in enemyCards)
+    	{
+    		enemy *= NUM_CLASSES;
+    		enemy += card;
+    	}
+
+    	// player cards: each class can have 5 (DECK_SIZE+1) different values
+    	int player = 0;
+    	for(int i=0; i<myCards.Length-1; i++)
+    	{
+    		player *= (DECK_SIZE + 1);
+    		player += myCards[i];
+    	}
+
+    	// Combine both. We need the max possible of one of them to convert
+    	// 2-indices to 1-index
+    	int state = enemy + player*enemyCombinations;
+    	//Debug.Log("STATE");
+    	//Debug.Log(enemy);
+    	//Debug.Log(player);
+    	//Debug.Log(state);
+
+    	return state;
+    }
+
+
+    private int ChooseAction()
+    {
+    	int action; 
+
+		// Decide if it is going to be a random action or
+		// according to Q table
+		if (Random.Range (0.0f, 1.0f) < epsilon) 	// Random action
+		{
+			// Random.Range with ints does not include the maximum
+			action = Random.Range (0, numActions);		
+		} 
+		else 	// Best action according to Q-table (column with highest value)
+		{
+			int colMax = 0;
+			for (int col = 1; col < numActions; col++)
+				if (qTable [state, col] > qTable [state, colMax])
+					colMax = col;
+
+			action = colMax;
+		}
+
+
+		// Reduce epsilon (to gradually reduce the random exploration)
+		if (epsilon > minEpsilon) { 
+			epsilon -= ((1.0f - minEpsilon) / (float)coolingSteps); 
+		}
+
+		return action;
+    }
+   
+
+    // Translate the int that encodes the action into an array with the
+    // cards that the agent is playing
+   	private int [] ActionToCards(int action)
+   	{
+   		//Debug.Log("ActionToCards");
+   		//Debug.Log(action);
+
+   		int [] cards = new int[numEnemyCards];  // Enemy and player show the same number of cards
+
+   		for(int i=numEnemyCards-1; i>=0; i--)
+   		{
+   			cards[i] = action % NUM_CLASSES;
+   			action  /= NUM_CLASSES;
+   		}
+
+   		//foreach(int x in cards)
+   		//	Debug.Log(x);
+
+   		return cards;
+   	}
+
+
+}
---
+++ CardsOfTheWild_A3/Assets/Scripts/GameRun.cs
@@ -0,0 +1,221 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameRun : MonoBehaviour
+{
+
+	// Management of sprites
+	private Object[] backgrounds;
+	private Object[] props;
+	private Object[] chars;
+
+	// Game management
+	private GameObject enemyCards;
+	private int [] enemyChars;	
+	private Agent agent;
+
+	private int NUM_ENEMY_CARDS = 3;
+	private int NUM_CLASSES     = 3;
+	private int DECK_SIZE       = 4;
+
+	// Rewards
+	private float RWD_ACTION_INVALID = -2.0f;
+	private float RWD_HAND_LOST      = -1.0f;
+	private float RWD_TIE            = -0.1f;
+	private float RWD_HAND_WON       =  1.0f;
+
+	// Other UI elements
+	private UnityEngine.UI.Text textDeck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        ///////////////////////////////////////
+        // Sprite management
+        ///////////////////////////////////////
+
+        // Load all prefabs
+        backgrounds = Resources.LoadAll("Backgrounds/");
+        props       = Resources.LoadAll("Props/");
+        chars       = Resources.LoadAll("Chars/");
+
+
+        ///////////////////////////////////////
+        // UI management
+        ///////////////////////////////////////
+        textDeck = GameObject.Find("TextDeck").GetComponent<UnityEngine.UI.Text>();
+
+
+        ///////////////////////////////////////
+        // Game management
+        ///////////////////////////////////////
+        enemyCards = GameObject.Find("EnemyCards");
+        enemyChars = new int[NUM_ENEMY_CARDS];
+
+        agent      = GameObject.Find("AgentManager").GetComponent<Agent>();
+
+        agent.Initialize();
+
+
+        ///////////////////////////////////////
+        // Start the game
+        ///////////////////////////////////////
+        StartCoroutine("GenerateTurn");
+
+
+        ///////////////////////////////////////
+        // Image generation
+        ///////////////////////////////////////
+    	//renderTexture = gameObject.GetComponent<Camera>().targetTexture;
+
+    	//imgWidth  = renderTexture.width;
+    	//imgHeight = renderTexture.height;
+
+        
+    }
+
+
+    // Generate a card on a given transform
+    // Return the label (0-2) of the card
+    private int GenerateCard(Transform parent)
+    {
+
+    	int idx = Random.Range(0, backgrounds.Length);
+    	Instantiate(backgrounds[idx], parent.position, Quaternion.identity, parent);
+
+
+    	idx               = Random.Range(0, props.Length);
+    	Vector3 position = new Vector3(Random.Range(-3.0f, 3.0f), Random.Range(-3.0f, 3.0f), -1.0f);
+   	  	Instantiate(props[idx], parent.position+position, Quaternion.identity, parent);
+
+    	idx         = Random.Range(0, chars.Length);
+    	position    = new Vector3(Random.Range(-3.0f, 3.0f), Random.Range(-3.0f, 3.0f), -2.0f);    	
+   	  	Instantiate(chars[idx], parent.position+position, Quaternion.identity, parent);
+
+   	  	// Determine label of the character, return it
+   	  	int label = 0;
+   	  	if(chars[idx].name.StartsWith("frog")) label = 1;
+   	  	else if(chars[idx].name.StartsWith("opposum")) label = 2;
+
+    	return label;
+    } 
+
+    // Generate another turn
+    IEnumerator GenerateTurn()
+    {	
+    	for(int turn=0; turn<100000; turn++) {
+
+	        ///////////////////////////////////////
+	        // Generate enemy cards
+	        ///////////////////////////////////////
+
+	    	// Destroy previous sprites (if any) and generate new cards
+	    	int c = 0;
+	    	foreach(Transform card in enemyCards.transform) {
+	    		foreach(Transform sprite in card) {
+	    			Destroy(sprite.gameObject);
+	    		}
+
+	    		enemyChars[c++] = GenerateCard(card);
+	    	}
+
+
+	        ///////////////////////////////////////
+	        // Generate player deck
+	        ///////////////////////////////////////
+	        int [] deck   = GeneratePlayerDeck();
+	        textDeck.text = "Deck: ";
+	        foreach(int card in deck)
+	        	textDeck.text += card.ToString() + "/";
+
+
+	        ///////////////////////////////////////
+	        // Tell the player to play
+	        ///////////////////////////////////////
+
+	        // IMPORTANT: wait until the frame is rendered so the player sees
+	        //            the newly generated cards (otherwise it will see the previous ones)
+	        yield return new WaitForEndOfFrame();
+
+	        int [] action = agent.Play(deck, enemyChars);
+
+	        textDeck.text += " Action:";
+	        foreach(int a in action)
+	        	textDeck.text += a.ToString() + "/";
+
+
+
+
+	        ///////////////////////////////////////
+	        // Compute reward
+	        ///////////////////////////////////////
+	        float reward = ComputeReward(deck, action);
+	        
+	        Debug.Log("Turn/reward: " + turn.ToString() + "->" + reward.ToString());
+
+	        agent.GetReward(reward);
+
+
+	        ///////////////////////////////////////
+	        // Manage turns/games
+	        ///////////////////////////////////////
+
+
+
+	    	yield return new WaitForSeconds(0.1f);
+
+    	}
+
+    }
+
+
+    // Auxiliary methods
+    private int [] GeneratePlayerDeck()
+    {
+    	int [] deck = new int [DECK_SIZE];
+
+    	for(int i=0; i<DECK_SIZE; i++)
+    	{
+    		deck[i] = Random.Range(0, NUM_CLASSES);  // high limit is exclusive so [0, NUM_CLASSES-1]
+    	}
+
+    	return deck;
+    }
+
+    // Compute the result of the turn and return the associated reward 
+    // given the cards selected by the agent (action)
+   	// deck -> array with the number of cards of each class the player has
+   	// action -> array with the class of each card played
+    private float ComputeReward(int [] deck, int [] action)
+    {
+    	// First check if the action is valid given the player's deck
+    	foreach(int card in action)
+    	{
+    		deck[card]--;
+    		if(deck[card] < 0)
+    			return RWD_ACTION_INVALID;
+    	}
+
+
+    	// Second see who wins
+    	int score = 0;
+    	for(int i=0; i<NUM_ENEMY_CARDS; i++)
+    	{
+    		if(action[i] != enemyChars[i])
+    		{
+    			if(action[i] > enemyChars[i] || action[i]==0 && enemyChars[i]==2)	
+    				score++;
+    			else
+    				score--;
+    		}
+    		
+    	}
+
+    	if(score == 0) return RWD_TIE;
+    	else if(score > 0) return RWD_HAND_WON;
+    	else return RWD_HAND_LOST;
+    }
+}
